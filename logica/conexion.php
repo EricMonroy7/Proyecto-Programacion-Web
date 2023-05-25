e78e3e533f6ed05e5398445bdfc3a43decbcd5a6
ericmonroy7 <ericmonroy01@aragon.unam.mx>
2023-05-24 20:49:48 -0600

    se modifico la conexion con la nueva base de datos, las contraseñas para la compatibilidad con el host

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,14 +1,9 @@
 <?php
 
-// $host_db = "localhost:3306";
-// $user_db = "id20310581_aaronico";
-// $pass_db = "Hw\&J=21JXiLsZ~<";
-// $db_name = "id20310581_test_php";
-
-$host_db = "localhost:3306";
-$user_db = "root";
-$pass_db = "";
-$db_name = "eliminar";
+$host_db = "localhost";
+$user_db = "id20311365_root";
+$pass_db = "123456789.1As";
+$db_name = "id20311365_eliminar";
 
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
