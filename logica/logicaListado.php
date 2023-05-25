bc4277e56b26b58f59dfed9573bade90f970bde5
ericmonroy7 <ericmonroy01@aragon.unam.mx>
2023-05-24 20:39:10 -0600

    Se agregaron los botones de actualizar y editar

--- logica/logicaListado.php
+++ logica/logicaListado.php
@@ -1,31 +1,40 @@
 <?php   
-       
-    $consulta_sql="SELECT * FROM persona  WHERE no_cuenta =" .$no_cuenta;
+       include "conexion.php";
+    $consulta_sql="SELECT * FROM persona";  
 
         $resultado=$conexion->query($consulta_sql);
         
         $count=mysqli_num_rows($resultado);
- ?>       
-        <table border="2px"> 
-            <tr>
+ ?>      
+ <link rel="stylesheet" href="../estilo_login.css">
+ <body class=fondo>
+        <table border="4px"> 
+            <tr class=Tabla>
 
                 <th>Nombre Usuario</th>
                 <th>Carrera</th>
                 <th>Numero de cuenta</th>
-                
                 <th>Direccion</th>
                 <th>Telefono</th>
                 <th>Email</th>
-        
                 <th>Fecha de registro</th>
                 <th>Permisos</th>
+                
             </tr>
+            </body>
+            <style>
+            .Tabla{
+    background-color: white;
+    border: 2px solid #E1E1E1;
+    }
+    </style>
      <?php   
             if ($count>0){
         
             while($row=mysqli_fetch_assoc($resultado) ){
         
-                echo "<tr>";
+                echo "<tr class=Tabla>";
+
                 echo "<td>".$row['nombre_usuario']."</td>";
                 echo "<td>".$row['carrera']."</td>";
                 echo "<td>".$row['no_cuenta']."</td>";
@@ -35,7 +44,6 @@
                 echo "<td>".$row['fecha_registro']."</td>";
                 echo "<td>".$row['permisos']."</td>";
                 
-
                 echo "</tr>";
             } 
             
@@ -48,8 +56,13 @@
         
         
         ?>
-        <a href="eliminar.php"><h2 >Eliminar Usuario</h2></a>    
-        
+        <STYLE>A {text-decoration: none;} </STYLE>
+        <center> 
+        <button><a href="eliminar.php"><h2>ELIMINAR USUARIO</h2></a></button> 
+        <button><a href='logica/salir.php'> <h2>SALIR</h2></a></button>
+        <button><a href='logica/actualizar.php'> <h2>ACTUALIZAR</h2></a></button>
+        <button><a href='logica/edicion.php'> <h2>EDITAR</h2></a></button>
+       </center> 
+       <br>
         </table>        
-
 
