5db2f9445f158bb8a58714a278be6743111e8f7c
ericmonroy7 <ericmonroy01@aragon.unam.mx>
2023-05-24 21:00:15 -0600

    Se agrego CSS y se agrego un bton para regresar

--- eliminar.php
+++ eliminar.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eliminar</title>
+    <link rel="stylesheet" href="../estilo_login.css">
+    <title>ELIMINAR</title>
 </head>
 <style>
     .titulo{
         text-align: center;
-        color: red;
-        border: 2px solid black;
+        color: GREEN;
+        border: 4px solid black;
+        background-color: rgb(59, 255, 163);
+     position: relative;
+   
+    padding: 17px;
+    width: 280px;
+    
     }
 
     .formulario{
@@ -19,16 +26,23 @@
         font-size: 2em;
     }
 </style>
-<body>
-    <h1   class="titulo" >Eliminar Usuario</h1>
+<STYLE>A {text-decoration: none;} </STYLE>
+
+<body class=fondo>
+   <center>  <h1  class="titulo" >ELIMINAR USUARIO</h1></center> 
     <form  class="formulario" action="logica/delete.php" method="post">
 
         <input type="text" name="id" placeholder="ID de Usuario">
         <br>
-        <button type="submit" >Eliminar Usuario</button>
-
+        <button type="submit" >ELIMINAR USUARIO</button>
+<br>
     </form>
-
-    <a href="Principal.php"><h2 >Regresar a listas</h2></a>
+    <br>
+    <br>
+    <br>
+    <center> 
+    <button><a href="../Principal.php"><h2 >REGRESAR A LISTAS</h2></a></button>
+    </center> 
+     <br>
 </body>
 </html>
